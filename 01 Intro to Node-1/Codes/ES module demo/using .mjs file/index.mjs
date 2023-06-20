c56ea082087dce4d0c851dda65a9d68fb68cf3d8
arvindpndit <arvindpandit9808@gmail.com>
2023-06-20 12:58:08 +0530

    ES module demo using .mjs file code added

---
+++ 01 Intro to Node-1/Codes/ES module demo/using .mjs file/index.mjs
@@ -0,0 +1,13 @@
+//const doCalculation = require("./alzebra");
+import doCalculation from "./alzebra.js";
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(doCalculation);
+//addition
+let addition = doCalculation.addition(num1, num2);
+console.log(addition);
+
+//division
+console.log(doCalculation.division(num1, num2));
